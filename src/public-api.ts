60911a31c8e926e598e0c0ed3d8c24492ec86cee
GarethBeer <gareth.beer1989@outlook.com>
2022-06-21 11:39:36 +0100

    removing export of deleted service

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-search-field
  */
 
-export * from './lib/ngx-search-field.service';
 export * from './lib/ngx-search-field.component';
 export * from './lib/ngx-search-field.module';
 export * from './lib/input-fitler.pipe';
